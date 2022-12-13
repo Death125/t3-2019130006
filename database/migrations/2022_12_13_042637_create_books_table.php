56a322cea506acc893d5344cac4b6d8eb09a204e
Death125 <viki.fernando2@gmail.com>
2022-12-13 11:34:36 +0700

    First commit

---
+++ database/migrations/2022_12_13_042637_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->judul();
+            $table->smallInteger('halaman', 4);
+            $table->string('kategori', 20);
+            $table->string('penerbit', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
